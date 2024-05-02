6ee89224a135fbccb1341928be472556d8601cb3
IvannaTorres <119988894+IvannaTorres@users.noreply.github.com>
2024-05-02 09:46:38 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, url_for, request, jsonify
+import threading
+from pyngrok import ngrok
 from text_sentiment_prediction import *
 
 #libreria para exponer la api a una url publica
-from flask_ngrok import run_with_ngrok
+
 
 app = Flask(__name__)
+port = "5000"
+
+public_url = ngrok.connect(port).public_url
+print(public_url)
+
+app.config["BASE_URL"] = public_url
 
 # correr la api en url publica
-run_with_ngrok(app)
+#run_with_ngrok(app)
 
 @app.route('/')
 #cambiar nombre de index a home
@@ -21,29 +29,19 @@ def home():
 def predict_emotion():
     
     # Obtener el texto ingresado del requerimiento POST.
-    input_text = request.json.get("text")  
+    
     
     if not input_text:
         # Respuesta para enviar si input_text está indefinido.
-        response = {
-                    "status": "error",
-                    "message": "¡Por favor, ingresa algún texto para predecir la emoción!"
-                  }
-        return jsonify(response)
+        
     else:  
-        predicted_emotion,predicted_emotion_img_url = predict(input_text)
+        
         
         # Respuesta para enviar si input_text no está indefinido.
-        response = {
-                    "status": "success",
-                    "data": {
-                            "predicted_emotion": predicted_emotion,
-                            "predicted_emotion_img_url": predicted_emotion_img_url
-                            }  
-                   }
+        
 
         # Enviar respuesta.         
-        return jsonify(response)
+        
 #funcion que lee el archivo csv con el hsitorial de predicciones y lo devuelve en un arreglo
 def show_entry():
     day_entry_list = pd.read_csv("/content/Clase134/static/assets/data_files/data_entry.csv")
@@ -92,7 +90,8 @@ def show_entry():
             "emotion_url": emotion_url_3
         }
     ]
-app.run()
+    
+threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
 
 
 
